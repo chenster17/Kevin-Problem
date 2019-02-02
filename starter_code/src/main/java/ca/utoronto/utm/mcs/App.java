bcd331b37a515c384a3c0642444a0d1b2cf76337
Zhao Chen <Zhao Chen@DESKTOP-8ABKAEH>
2019-02-02 18:45:17 -0500

    Added the other files, will start to branch to work on each thing

--- starter_code/src/main/java/ca/utoronto/utm/mcs/App.java
+++ starter_code/src/main/java/ca/utoronto/utm/mcs/App.java
@@ -10,6 +10,13 @@ public class App
     public static void main(String[] args) throws IOException
     {
         HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", PORT), 0);
+        server.createContext("/api/v1/addActor", new Actor());
+        server.createContext("/api/v1/getActor", new Actor());
+        server.createContext("/api/v1/addMovie", new Movie());
+        server.createContext("/api/v1/getMovie", new Movie());
+        server.createContext("/api/v1/addRelationship", new Relationship());
+        server.createContext("/api/v1/hasRelationship", new Relationship());
+        
         server.start();
         System.out.printf("Server started on port %d...\n", PORT);
     }
