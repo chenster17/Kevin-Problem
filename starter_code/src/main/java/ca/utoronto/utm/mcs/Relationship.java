23b88dabc2148b2220880e4066db987be50d565a
Jack He <jack.he@mail.utoronto.ca>
2019-02-06 22:32:46 -0500

    -commit of relationship.java

---
+++ starter_code/src/main/java/ca/utoronto/utm/mcs/Relationship.java
@@ -0,0 +1,15 @@
+package ca.utoronto.utm.mcs;
+
+import java.io.IOException;
+
+public class Relationship implements HttpHandler
+{
+	public void handle(HttpExchange r){
+		try {
+            if (r.getRequestMethod().equals("GET")) {
+                handleGet(r);
+            } else if (r.getRequestMethod().equals("PUT")) {
+                handlePut(r);
+            }
+	}
+}
