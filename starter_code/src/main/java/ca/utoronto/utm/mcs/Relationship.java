e25158ca40f632b25c11b179268dbe014be3789a
Jack He <jack.he@mail.utoronto.ca>
2019-02-06 23:17:31 -0500

    finished creating 400 error handlers

--- starter_code/src/main/java/ca/utoronto/utm/mcs/Relationship.java
+++ starter_code/src/main/java/ca/utoronto/utm/mcs/Relationship.java
@@ -1,15 +1,24 @@
 package ca.utoronto.utm.mcs;
 
 import java.io.IOException;
+import java.io.OutputStream;
+
+import org.json.*;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
 
 public class Relationship implements HttpHandler
 {
 	public void handle(HttpExchange r){
 		try {
             if (r.getRequestMethod().equals("GET")) {
-                handleGet(r);
+            	return;
+                //handleGet(r);
             } else if (r.getRequestMethod().equals("PUT")) {
-                handlePut(r);
+            	return;
+                //handlePut(r);
             }
 	}
 }
